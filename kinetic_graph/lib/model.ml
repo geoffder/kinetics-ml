a00b111e5a20956b3b965aceceb22fd931433d06
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-08-03 14:00:51 -0700

    Excise Diffusion module to own file.

--- kinetic_graph/lib/model.ml
+++ kinetic_graph/lib/model.ml
@@ -1,11 +1,6 @@
 open Base
 open Graph_builds
 
-module FloatUtil = struct
-  (* Scientific notation convenience operator. *)
-  let  ( *^. ) a b = a *. 10. **. b
-end
-
 module Edge = struct
   type t = { name : string
            ; from : string
@@ -70,74 +65,6 @@ module Graph = struct
   let step dt edges flows agonist state =
     commit dt agonist flows state
     |> apply edges state
-end
-
-module Diffusion = struct
-  open FloatUtil
-
-  type t = float list
-
-  type spec2D = { n_mols : float
-                ; coef : float
-                ; height : float
-                ; radius : float
-                }
-
-  type spec3D = { n_mols : float
-                ; coef : float
-                ; radius : float
-                ; alpha : float
-                ; lambda : float
-                }
-
-  type space = TwoD of spec2D | ThreeD of spec3D
-
-  let glut_2D r = TwoD { n_mols = 4700.
-                       ; coef   = 7.6 *^. -10.
-                       ; height = 20. *^. -9.
-                       ; radius = r
-                       }
-
-  let ach_2D r = TwoD { n_mols = 10000.
-                      ; coef   = 4. *^. -10.
-                      ; height = 20. *^. -9.
-                      ; radius = r
-                      }
-
-  let glut_3D  r = ThreeD { n_mols = 4700.
-                          ; coef   = 7.6 *^. -10.
-                          ; alpha = 0.12
-                          ; lambda = 1.55
-                          ; radius = r
-                          }
-
-  let ach_3D r = ThreeD { n_mols = 10000.
-                        ; coef   = 4. *^. -10.
-                        ; alpha = 0.12
-                        ; lambda = 1.55
-                        ; radius = r
-                        }
-
-  let calc2D { n_mols = m; coef = d; height = h; radius = r } = fun t ->
-    let moles = m /. (6.02 *^. 23.) in
-    let d' = d *. t in
-    let x = moles /. (4. *. h *. Float.pi *. d') in
-    let y = Float.exp ((-.r) **. 2. /. (4. *. d')) in
-    x *. y
-
-  let calc3D { n_mols = m; coef = d; radius = r; alpha = a; lambda = l } = fun t ->
-    let moles = m /. (6.02 *^. 23.) in
-    let d' = t *. (d /. l **. 2.) in
-    let x = moles /. (8. *. a *. (Float.pi *. d') **. 1.5 ) in
-    let y = Float.exp ((-.r) **. 2. /. (4. *. d)) in
-    x *. y
-
-  (* Temporal profile of agonist concentration. *)
-  let get_profile space_spec time =
-    let calc = match space_spec with
-      | TwoD spec -> calc2D spec
-      | ThreeD spec -> calc3D spec in
-    List.map ~f:calc time
 end
 
 module Rig = struct
