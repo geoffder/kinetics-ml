ba786437346d97cf218b8965235688d33e1690bc
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-08-06 16:39:51 -0700

    Test plotting multiple lines. TODO abstract out.

--- kinetic_graph/lib/experiments.ml
+++ kinetic_graph/lib/experiments.ml
@@ -21,19 +21,28 @@ let set_plot_defaults h =
   Plot.set_font_size h 10.;
   Plot.set_pen_size h 3.
 
+(* TODO: It works, so abstract and break up. *)
 let test_plot () =
-  let rig =
+  let rig_prox =
     Diffusion.ach_2D 0.
-    |> Rig.build @@ Alpha7.make ~on_multi:1. ~desens_div:1. () in
-  let data = rig |> Rig.run |> Map.map ~f:mat_of_1d_list in
+    |> Rig.build @@ Alpha7.make ~on_multi:1. ~desens_div:4. () in
+  let rig_dist =
+    Diffusion.(ach_2D (1.1 *^. -.6.))
+    |> Rig.build @@ Alpha7.make ~on_multi:1. ~desens_div:4. () in
+  let data_prox = rig_prox |> Rig.run |> Map.map ~f:mat_of_1d_list in
+  let data_dist = rig_dist |> Rig.run |> Map.map ~f:mat_of_1d_list in
   (* Time begins as one dt in, so add t0. *)
-  let time = rig.time |> List.cons 0. |> mat_of_1d_list in
-  let h = Plot.create "alpha7_r0.png" in
+  let time = rig_prox.time |> List.cons 0. |> mat_of_1d_list in
+  let h = Plot.create "alpha6_prox_distal.png" in
   set_plot_defaults h;
-  Plot.set_title h "Alpha7, radius = 0";
+  Plot.set_title h "Alpha6";
   Plot.set_xlabel h "Time (ms)";
   Plot.set_ylabel h "Probability";
   Plot.(plot ~h
-          ~spec:[ RGB (255,0,0);  LineStyle 1 ]
-          time (open_prob data));
+          ~spec:[ RGB (255, 0, 0);  LineStyle 1 ]
+          time (open_prob data_prox));
+  Plot.(plot ~h
+          ~spec:[ RGB (0, 0, 255);  LineStyle 1 ]
+          time (open_prob data_dist));
+  Plot.legend_on h [| "Proximal"; "Distal" |];
   Plot.output h
